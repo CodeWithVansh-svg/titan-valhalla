ff3a33946b30a6f0de8581d9ffb2b1639e80a374
Vansh Dudhe <dudhevansh8@gmail.com>
2026-08-04 15:59:24 +0530

    Remove commented-out code in supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,9 @@
-// js/supabase.js
-
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://zodcxxowdneqqbdiqwic.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_bnlfWGCM0Kz-zOwLJdRU0w_tAswB4jc";
+const SUPABASE_ANON_KEY =
+"sb_publishable_bnlfWGCM0Kz-zOwLJdRU0w_tAswB4jc";
 
 export const supabase = createClient(
     SUPABASE_URL,
