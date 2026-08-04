385c5e8950e2955f5c2a5451f8622dd64427b821
Vansh Dudhe <dudhevansh8@gmail.com>
2026-08-04 16:27:45 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-import { loadProfile, logout } from "./js/auth.js";
-import { supabase } from "./js/supabase.js";
+import {
+    loadProfile,
+    logout
+} from "./js/auth.js";
+
+import {
+    supabase
+} from "./js/supabase.js";
 const STORAGE_KEY = 'login-users-db';
 const CURRENT_USER_KEY = 'logged-in-user';
 const ADMINS = [
